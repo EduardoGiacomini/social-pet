420061603154d761918d870a711ce7d858b2b8e7
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-09-23 09:25:44 -0300

    feat: 🎸 Implementação parcial da página de boas-vindas

--- app/src/globals/router/index.js
+++ app/src/globals/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
-
-import Greetings from '@/views/Greetings/Greetings'
 
 Vue.use(Router)
 
@@ -10,23 +7,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
+    /* {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/greetings',
       name: 'greetings',
       component: Greetings
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../../views/About.vue')
-    }
+    } */
   ]
 })
--- app/src/views/Greetings/Greetings.vue
+++ app/src/views/Greetings/Greetings.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Greetings</h1>
+    <div class="hero__box">
+      <img class="hero__icon" alt="Social Pet Hero" src="../../assets/icons/hero.svg">
+    </div>
+    <div class="container">
+      <h1 class="title center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
+      <p class="subtitle center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
+    </div>
   </div>
 </template>
