6d01091e1d26221c8d20be638e55c12a7badfdde
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-09-24 13:22:13 -0300

    feat: 🎸 Finalizando botão da página de boas-vindas

--- app/src/views/Greetings/Greetings.vue
+++ app/src/views/Greetings/Greetings.vue
@@ -4,8 +4,12 @@
       <img class="hero__icon" alt="Social Pet Hero" src="../../assets/icons/hero.svg">
     </div>
     <div class="container">
-      <h1 class="title center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
-      <p class="subtitle center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
+      <div class="mb4">
+        <h1 class="title center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
+        <p class="subtitle center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
+      </div>
+      <button class="button full-width mb2">Entrar</button>
+      <p class="subtitle center-text">Não tem uma conta? <router-link class="link" to="/">Cadastre-se</router-link></p>
     </div>
   </div>
 </template>
