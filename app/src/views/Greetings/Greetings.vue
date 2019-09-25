8614ef6dc0f65678759a21ac353951c8dbf74453
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-09-25 11:07:29 -0300

    refactor: 💡 Alterando botão para link

--- app/src/views/Greetings/Greetings.vue
+++ app/src/views/Greetings/Greetings.vue
@@ -8,7 +8,7 @@
         <h1 class="title center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
         <p class="subtitle center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
       </div>
-      <button class="button full-width mb2">Entrar</button>
+      <router-link class="button full-width mb2" to="/">Entrar</router-link>
       <p class="subtitle center-text">Não tem uma conta? <router-link class="link" to="/">Cadastre-se</router-link></p>
     </div>
   </div>
