fc22625bbb2801ae0b438e1c303e3b0663207bf0
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-09-29 22:40:13 -0300

    style: 💄 Adicionando animações nos elementos

--- app/src/views/public/Greetings/Greetings.vue
+++ app/src/views/public/Greetings/Greetings.vue
@@ -5,11 +5,13 @@
     </div>
     <div class="container">
       <div class="mb4">
-        <h1 class="title center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
-        <p class="subtitle center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
+        <h1 class="title move-in-right center-text">Compartilhe momentos inesquecíveis ao lado do seu pet</h1>
+        <p class="subtitle move-in-left center-text">Curta e compartilhe momentos vivenciados ao lado do seu pet com amigos e familiares.</p>
       </div>
-      <router-link class="button full-width mb2" to="/">Entrar</router-link>
-      <p class="subtitle center-text">Não tem uma conta? <router-link class="link" to="/create-user">Cadastre-se</router-link></p>
+      <div class="move-in-top">
+        <router-link class="button full-width mb2" to="/">Entrar</router-link>
+        <p class="subtitle center-text">Não tem uma conta? <router-link class="link" to="/create-user">Cadastre-se</router-link></p>
+      </div>
     </div>
   </div>
 </template>
