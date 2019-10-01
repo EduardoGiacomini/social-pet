d183b23ba3df90923c67ea54f4b0271e9a01b483
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-10-01 11:44:47 -0300

    feat: 🎸 Construindo componente de steps

--- app/src/views/public/Tour/Tour.vue
+++ app/src/views/public/Tour/Tour.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="step">
+      <span class="step-stage"></span>
+      <span class="step-stage"></span>
+      <span class="step-stage step-stage--active"></span>
+    </div>
   </div>
 </template>
 
@@ -10,18 +15,21 @@ export default {
     return {
       tour: [
         {
+          active: true,
           step: 1,
           img: 'caminho.svg',
           title: 'Mostre o que o seu amiguinho pode fazer',
           subtitle: 'Seja morder os seus chinelos, correr atrás do rabo ou dormir o dia todo, qualquer coisa está valendo.'
         },
         {
+          active: false,
           step: 2,
           img: 'caminho.svg',
           title: 'Curta postagens de seus amigos',
           subtitle: 'Curta e conheça pessoas e seus pets próximos de você.'
         },
         {
+          active: false,
           step: 3,
           img: 'caminho.svg',
           title: 'Deixe os momentos mais especiais',
