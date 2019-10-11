9b925b7c59eab2458a1f6238553f4f758cb896db
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-10-11 20:09:38 -0300

    feat: 🎸 Criando menu de hamburguer gourmet

--- app/src/globals/router/public-routes.js
+++ app/src/globals/router/public-routes.js
@@ -2,6 +2,9 @@ import Greetings from '@/views/public/Greetings/Greetings.vue'
 import Signup from '@/views/public/Signup/Signup.vue'
 import Tour from '@/views/public/Tour/Tour.vue'
 
+// Todo: criar rota privada
+import Home from '@/views/private/Home/Home.vue'
+
 export default [
   {
     path: '/',
@@ -17,5 +20,10 @@ export default [
     path: '/criar-conta',
     name: 'signup',
     component: Signup
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
   }
 ]
---
+++ app/src/views/private/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <Menu/>
+  </div>  
+</template>
+
+<script>
+import Menu from '@/commons/components/Menu.vue'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
